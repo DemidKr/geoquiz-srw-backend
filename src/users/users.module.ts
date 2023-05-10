89541291d5fe24bc7109eeac2f0ef26c6406e462
DemidKr <demid.krasnovidov@gmail.com>
2023-05-10 19:31:03 +0300

    Added authorization with access and refresh token

---
+++ src/users/users.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import {SequelizeModule} from "@nestjs/sequelize";
+import {User} from "./users.model";
+
+@Module({
+    providers: [UsersService],
+    imports: [
+        SequelizeModule.forFeature([User])
+    ],
+    exports: [UsersService]
+})
+export class UsersModule {}
---
+++ src/users/users.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import {User} from "./users.model";
+import {InjectModel} from "@nestjs/sequelize";
+import {CreateUserDto} from "../auth/dto/create-user.dto";
+import {LoginUserDto} from "../auth/dto/login-user.dto";
+
+@Injectable()
+export class UsersService {
+    constructor(@InjectModel(User) private userRepository: typeof User) {}
+
+    async login(loginUserDto: LoginUserDto): Promise<User | null> {
+        const user = await this.userRepository.findOne({
+            where: {username: loginUserDto.username}
+        });
+
+        if(!user) {
+            return null;
+        }
+
+        return user as User;
+    }
+
+    async registration(createUserDto: CreateUserDto): Promise<User | null> {
+        const existingUser = await this.userRepository.findOne({
+            where: {username: createUserDto.username}
+        });
+
+        if (existingUser) {
+            return null;
+        }
+
+        const createdUser = await this.userRepository.create(createUserDto)
+        return createdUser.save()
+    }
+
+    async createUser(createUserDto: CreateUserDto): Promise<User> {
+        const user = await this.userRepository.create(createUserDto)
+        return user.save();
+    }
+
+    async findOne(username: string): Promise<User> {
+        return await this.userRepository.findOne({
+            where: {username}
+        });
+    }
+}
